a6210d8cd3b231dd61b5469fb9957708b3c1286b
Nicole Travers <nicole@homesnappers.com>
2023-06-20 15:20:26 -0500

    Created new Hello component as a function component

--- First_Component/index.js
+++ First_Component/index.js
@@ -1,12 +1,20 @@
-class Hello extends React.Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello there!</h1>
-				<h2>Hello there!</h2>
-			</div>
-		);
-	}
-}
+//class Hello extends React.Component {
+//	render() {
+//		return (
+//			<div>
+//				<h1>Hello there!</h1>
+//				<h2>Hello there!</h2>
+ //			</div>
+//		);
+//	}
+//}
 
+function Hello(){
+	return (
+		<div>
+			<h1>Hello there!</h1>
+			<h2>Hello there!</h2>
+		</div>
+	);
+}
 ReactDOM.render(<Hello />, document.getElementById('root'));
