78f2893a343271d01e69c90e0d30767c10801b20
Nicole Travers <nicole@homesnappers.com>
2023-06-23 10:55:47 -0500

    created function and component that randomly picks number

--- react-bootcamp-materials/02-introducing-jsx/code/03_JSX_Conditionals/index.js
+++ react-bootcamp-materials/02-introducing-jsx/code/03_JSX_Conditionals/index.js
@@ -1,4 +1,4 @@
-function getNum() {
+/*function getNum() {
 	return Math.floor(Math.random() * 10) + 1;
 }
 class NumPicker extends React.Component {
@@ -23,4 +23,17 @@ class NumPicker extends React.Component {
 	}
 }
 
-ReactDOM.render(<NumPicker />, document.getElementById('root'));
+ReactDOM.render(<NumPicker />, document.getElementById('root'));*/
+
+function getNum(){
+	return Math.floor(Math.random() * 10) + 1;
+}
+class NumPicker extends React.Component{
+	render() {
+		return (
+			<h1>Your number is {getNum()}</h1>
+		)
+	}
+}
+
+ReactDOM.render(<NumPicker/>, document.getElementById('root'));
