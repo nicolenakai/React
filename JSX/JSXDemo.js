c672ab7b5cae675f9fd8586cc303bd9392c0e18f
Nicole Travers <nicole@homesnappers.com>
2023-06-23 10:33:25 -0500

    created Demo component thhat displays an image

--- JSX/JSXDemo.js
+++ JSX/JSXDemo.js
@@ -1,11 +1,12 @@
 class JSXDemo extends React.Component {
-    render() {
-      return (
+   render(){
+    return (
         <div>
-            <h1>HI!</h1>
+            <h1>My Image!</h1>
+            <img src="https://images.unsplash.com/photo-1547626740-02cb6aed9ef8?ix" />
         </div>
-      );
-    }
+    )
+   } 
 }
 
-ReactDOM.render(<JSXDemo />, document.getElementById('root'));
+ReactDOM.render(<JSXDemo/>, document.getElementById('root'));
