74c8bb8d6dd4425fe601045f2bf9e4b832afce74
Nicole Travers <nicole@homesnappers.com>
2023-06-23 09:48:22 -0500

    downloaded the files

---
+++ JSX/JSXDemo.js
@@ -0,0 +1,11 @@
+class JSXDemo extends React.Component {
+    render() {
+      return (
+        <div>
+            <h1>HI!</h1>
+        </div>
+      );
+    }
+}
+
+ReactDOM.render(<JSXDemo />, document.getElementById('root'));
