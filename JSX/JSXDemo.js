0d438df6ed5f035c6e9c0dd553767630a9ff5861
Nicole Travers <nicole@homesnappers.com>
2023-06-23 10:40:34 -0500

    embedding JavaScript in JSX, created random mood picker

--- JSX/JSXDemo.js
+++ JSX/JSXDemo.js
@@ -1,9 +1,12 @@
+function getMood(){
+    const moods = ['Angry','Hungry', 'Silly', 'Quiet', 'Paranoid'];
+    return moods[Math.floor(Math.random() * moods.length)];
+}
 class JSXDemo extends React.Component {
    render(){
     return (
         <div>
-            <h1>My Image!</h1>
-            <img src="https://images.unsplash.com/photo-1547626740-02cb6aed9ef8?ix" />
+            <h1>My Current Mood is: {getMood()} </h1>
         </div>
     )
    } 
