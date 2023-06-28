1f2aa9bf5cd3b917343d58bda0cb7ed5f598a347
Nicole Travers <nicole@homesnappers.com>
2023-06-28 07:41:45 -0500

    changed the conditional

--- react-bootcamp-materials/03-props-and-more/code/03_Slots_Exercise/Machine1.js
+++ react-bootcamp-materials/03-props-and-more/code/03_Slots_Exercise/Machine1.js
@@ -1,10 +1,9 @@
 class Machine extends React.Component {
     render() {
-        const winner = (this.props.s1 === this.props.s2) && (this.props.s2 === this.props.s3);
       return (
         <div>
            <p>{this.props.s1} {this.props.s2} {this.props.s3}</p>
-           <p>{winner ? 'Winner!' : 'Loser!'}</p>
+           <p>{(this.props.s1 === this.props.s2) && (this.props.s2 === this.props.s3) ? 'Winner!' : 'Loser!'}</p>
         </div>
       );
     }
