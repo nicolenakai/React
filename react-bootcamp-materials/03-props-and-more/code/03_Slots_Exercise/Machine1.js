53f6225d3e8c16331bed32774de7fc535b95f683
Nicole Travers <nicole@homesnappers.com>
2023-06-28 07:02:44 -0500

    created three slot machine components and their props

---
+++ react-bootcamp-materials/03-props-and-more/code/03_Slots_Exercise/Machine1.js
@@ -0,0 +1,5 @@
+class Machine extends React.Component {
+    render() {
+      return <p>{this.props.s1} {this.props.s2} {this.props.s3}</p>;
+    }
+  }
---
+++ react-bootcamp-materials/03-props-and-more/code/03_Slots_Exercise/index1.js
@@ -0,0 +1,26 @@
+class App extends React.Component {
+	render() {
+	  return (
+		<div>
+            <h1>Slot Machines!</h1>
+		    <Machine 
+                s1="x" 
+                s2="y" 
+                s3="y"
+		    />
+            <Machine 
+                s1="y" 
+                s2="y" 
+                s3="y"
+            />
+            <Machine 
+                s1="x" 
+                s2="y" 
+                s3="z"
+            />	  
+		</div>
+	  )
+	}
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
