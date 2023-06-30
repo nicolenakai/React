73460fa8dc6e8fbc54fe6b9f0c370f323003d1c5
Nicole Travers <nicole@homesnappers.com>
2023-06-30 10:28:42 -0500

    used css to style slot machine

--- react-bootcamp-materials/03-props-and-more/code/06_Styling_React/index.js
+++ react-bootcamp-materials/03-props-and-more/code/06_Styling_React/index.js
@@ -1,4 +1,4 @@
-class App extends React.Component {
+/*class App extends React.Component {
 	render() {
 		return (
 			<div>
@@ -10,4 +10,26 @@ class App extends React.Component {
 	}
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));*/
+
+class App extends React.Component {
+	render() {
+	  return (
+		<div>
+		  <h1>Slot Machines!</h1>
+		  <Machine
+			s1="🍒"
+			s2="🍒"
+			s3="🍒"
+		  />
+		  <Machine
+			s1="🍒"
+			s2="🍒"
+			s3="🍊"
+		  />
+		</div>
+	  )
+	}
+  }
+  
+  ReactDOM.render(<App />, document.getElementById('root'))
