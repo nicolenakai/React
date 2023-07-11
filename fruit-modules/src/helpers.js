57f0c8a556a3f33c36370bd8c96be9fd1229187b
Nicole Travers <nicole@homesnappers.com>
2023-07-11 09:32:30 -0500

    everything's working in the console

--- fruit-modules/src/helpers.js
+++ fruit-modules/src/helpers.js
@@ -6,9 +6,9 @@ function choice(items){
 function remove(items,item){
     for(let i=0; i<items.length; i++){
         if(items[i] == item){
-            return [...items.slic(0,i), ...items.slice(i+1)];
+            return [...items.slice(0,i), ...items.slice(i+1)];
         }
     }
 }
 
-export default {choice, remove};
+export {choice, remove};
--- fruit-modules/src/index.js
+++ fruit-modules/src/index.js
@@ -1,3 +1,9 @@
 import foods from './foods';
-import {choice, remove} from './helpers'
+import {choice, remove} from './helpers';
 
+let fruit = choice(foods);
+console.log(`I'd like one ${fruit}, please.`);
+console.log(`Here you go: ${fruit}`);
+console.log(`Delicious! May i have another?`);
+let leftover = remove(foods,fruit);
+console.log(`I'm sorry, we're all out. We have ${leftover.length} left`);
